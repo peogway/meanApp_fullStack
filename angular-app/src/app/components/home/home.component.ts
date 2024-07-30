24853299c55ced0579b281ddf2dd431427933189
peogway <duchungbeo2004@gmail.com>
2024-07-30 17:56:17 +0300

    update fontend

--- angular-app/src/app/components/home/home.component.ts
+++ angular-app/src/app/components/home/home.component.ts
@@ -1,11 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  checkUser() {
+    if (localStorage.getItem('user') || localStorage.getItem('id_token'))
+      return true;
+    return false;
+  }
+}
